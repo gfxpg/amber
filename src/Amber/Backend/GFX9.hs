ae16af025bcdf4e3d4eca06ed52da963f59614d9
timlathy <tim.labushev@gmail.com>
2021-08-09 11:06:15 +0300

    keep a list of instructions instead of emitting them right away for later analysis

--- src/Amber/Backend/GFX9.hs
+++ src/Amber/Backend/GFX9.hs
@@ -5,46 +5,58 @@ import Amber.Program
 import Control.Monad.State
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Builder as BB
+import Data.ByteString.Short (ShortByteString)
 import Data.List (intersperse)
+import Data.Sequence (Seq, (|>))
+import qualified Data.Sequence as Seq
 import GHC.Stack (HasCallStack)
 
 assemble :: LlvmAsm -> Gfx9Program () -> IO ByteString
 assemble llvm (Gfx9Program p) = do
-  program <- runStateT p $ Gfx9Context {gfx9Llvm = llvm}
+  (_, ctx) <- runStateT p $ Gfx9Context {gfx9Instructions = Seq.empty}
+  forM_ (gfx9Instructions ctx) $ \i ->
+    emitAsm llvm $ emitInstruction i
   assembleElf llvm
 
-data Gfx9Context = Gfx9Context {gfx9Llvm :: LlvmAsm}
+emitInstruction :: Gfx9Instruction -> BB.Builder
+emitInstruction (Gfx9Instruction opcode ops) =
+  BB.shortByteString opcode <> " " <> mconcat (intersperse ", " (emitOp <$> ops))
+  where
+    emitOp (Gfx9Reg r) = BB.stringUtf8 r
+    emitOp (Gfx9Lit l) = BB.intDec l
+    emitOp (Gfx9Label l) = BB.stringUtf8 l
+emitInstruction (Gfx9InstructionLabel label) =
+  BB.stringUtf8 label <> ":"
+
+gfx9Instruction :: ShortByteString -> [Gfx9Operand] -> Gfx9Program ()
+gfx9Instruction opcode ops =
+  modify (\s -> s {gfx9Instructions = gfx9Instructions s |> Gfx9Instruction opcode ops})
+
+gfx9InsertLabel :: String -> Gfx9Program ()
+gfx9InsertLabel l =
+  modify (\s -> s {gfx9Instructions = gfx9Instructions s |> Gfx9InstructionLabel l})
+
+data Gfx9Context = Gfx9Context {gfx9Instructions :: Seq Gfx9Instruction}
+
+data Gfx9Instruction = Gfx9Instruction ShortByteString [Gfx9Operand]
+  | Gfx9InstructionLabel String
 
 newtype Gfx9Program a = Gfx9Program (StateT Gfx9Context IO a)
   deriving (Functor, Applicative, Monad, MonadFail, MonadState Gfx9Context, MonadIO)
 
-data Gfx9Operand = Gfx9Reg String | Gfx9Lit Int
+data Gfx9Operand = Gfx9Reg String | Gfx9Lit Int | Gfx9Label String
 
-data Gfx9Node = Gfx9Operand Gfx9Operand
+instance Program Gfx9Program Gfx9Operand where
+  reg s = return $ Gfx9Reg s
+  lit l = Gfx9Lit l
+  move a b = gfx9Instruction "s_mov_b32" [a, b]
 
-gfx9Emit :: BB.Builder -> Gfx9Program ()
-gfx9Emit bb = do
-  llvm <- gets gfx9Llvm
-  liftIO $ emitAsm llvm bb
-
-instance IntOps Gfx9Program Gfx9Node where
+instance IntOps Gfx9Program Gfx9Operand where
   --   throwError $ "e:\n" <> prettyCallStack callStack
-  add (Gfx9Operand dst) (Gfx9Operand src0) (Gfx9Operand src1) =
-    gfx9Emit $ "s_add_u32 " <> emit dst <> ", " <> emit src0 <> ", " <> emit src1
+  add dst@(Gfx9Reg _) src0 src1 =
+    gfx9Instruction "s_add_u32" [dst, src0, src1]
   add _ _ _ = fail "Invalid operands"
 
-instance ControlFlow Gfx9Program Gfx9Node where
-  label l n = gfx9Emit (BB.string8 l <> ":") >> n
-  jump l = gfx9Emit ("s_branch " <> BB.string8 l)
-
-instance Emit Gfx9Operand where
-  emit (Gfx9Reg r) = BB.string8 r
-  emit (Gfx9Lit l) = BB.intDec l
-
-instance Emit Gfx9Node where
-  emit (Gfx9Operand o) = emit o
-
-instance Program Gfx9Program Gfx9Node where
-  reg s = return $ Gfx9Operand $ Gfx9Reg s
-  move a b = gfx9Emit $ "s_mov_b32 " <> emit a <> ", " <> emit b
-  lit l = Gfx9Operand $ Gfx9Lit l
+instance ControlFlow Gfx9Program Gfx9Operand where
+  label l n = gfx9InsertLabel l >> n
+  jump l = gfx9Instruction "s_branch" [Gfx9Label l]
